80ec3c72f4f10f95f9d7b2231d22a60f97b1588c
Poroshin Ivan <poroshin-ivan@yandex-team.ru>
2018-05-04 03:46:40 +0500

    Espresso test

---
+++ app/src/androidTest/java/sample/codequality/view/MainActivityTest.java
@@ -0,0 +1,29 @@
+package sample.codequality.view;
+
+import android.support.test.rule.ActivityTestRule;
+import android.support.test.runner.AndroidJUnit4;
+
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import sample.codequality.R;
+
+import static android.support.test.espresso.Espresso.onView;
+import static android.support.test.espresso.action.ViewActions.click;
+import static android.support.test.espresso.assertion.ViewAssertions.matches;
+import static android.support.test.espresso.contrib.RecyclerViewActions.actionOnItemAtPosition;
+import static android.support.test.espresso.matcher.ViewMatchers.withId;
+import static android.support.test.espresso.matcher.ViewMatchers.withText;
+
+@RunWith(AndroidJUnit4.class)
+public class MainActivityTest {
+    @Rule
+    public ActivityTestRule<MainActivity> mActivityRule = new ActivityTestRule<>(MainActivity.class);
+
+    @Test
+    public void numpadButtons_areClickable() {
+        onView(withId(R.id.calculator_numpad)).perform(actionOnItemAtPosition(0, click()));
+        onView(withId(R.id.calculator_display)).check(matches(withText("7")));
+    }
+}
